0a95d8a903c13d3bc432b16440059a9fb38fd878
Cenans_Pc <emrcnn774@gmail.com>
2022-10-05 15:40:55 +0300

    GridLayoutPhotoGalery
    Photo gallery construction with gridlayout.

---
+++ gridlayout/app/src/main/java/com/example/gridlayout/FullScreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.gridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullScreenActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen);
+
+
+        imageView = findViewById(R.id.image_view);
+
+
+        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Tam Ekran Resim");
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+     imageView.setImageResource(imageAdapter.imageArray[position]);
+    }
+}
